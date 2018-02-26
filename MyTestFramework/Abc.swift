0bb2b9b28692ef84ef79b25f3c050d23c88c4cff
ybin <zyb0807@gmail.com>
2018-02-26 16:17:22 +0800

    Fixed

---
+++ MyTestFramework/Abc.swift
@@ -0,0 +1,15 @@
+//
+//  Abc.swift
+//  MyTestFramework
+//
+//  Created by 赵永斌 on 2018/2/26.
+//  Copyright © 2018年 赵永斌. All rights reserved.
+//
+
+import UIKit
+
+public class Abc: NSObject {
+    public func printMethod() {
+        print("Using frame work printing text")
+    }
+}
---
+++ MyTestFramework/MyTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyTestFramework.h
+//  MyTestFramework
+//
+//  Created by 赵永斌 on 2018/2/26.
+//  Copyright © 2018年 赵永斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyTestFramework.
+FOUNDATION_EXPORT double MyTestFrameworkVersionNumber;
+
+//! Project version string for MyTestFramework.
+FOUNDATION_EXPORT const unsigned char MyTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyTestFramework/PublicHeader.h>
+
+
